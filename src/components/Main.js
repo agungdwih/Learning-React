34a3551f27a589506d1a605b21d17d74ac128398
agungdwih <agungdwi.hartanto3@gmail.com>
2023-03-23 23:12:59 +0700

    next

--- src/components/Main.js
+++ src/components/Main.js
@@ -26,12 +26,7 @@ class Main extends Component{
                     <td>Otto</td>
                     <td>@mdo</td>
                     </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    </tr>
+                    
 
                 </tbody>
             </table>
